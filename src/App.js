a5725567c0a81828df33bd3f9f71862f91976e91
Gregory <andrievskiy.g@yandex.ru>
2019-03-20 00:31:29 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Router, Route } from "react-router";
+import { createBrowserHistory } from 'history';
+
 import './App.css';
 
+import { StartPage } from './pages/StartPage/StartPage';
+
+const customHistory = createBrowserHistory();
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router history = { customHistory } >
+                <div className="App">
+                    <header className="App-header">
+                        <Route exact path="/" component={StartPage} />
+                    </header>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/pages/StartPage/StartPage.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export class StartPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                YOU ARE IN PROFILE NOW!!!!! XXXTENTACTIONXXX
+            </div>
+        )
+    }
+}
